802fbc5dd1685cd8d7207e93e0af8a10508a849e
Eddier511 <124939498+Eddier511@users.noreply.github.com>
2025-11-05 22:21:59 -0600

    cambuo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Configuración para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: "/proyecto_constructora_winsemart/", // 👈 importante para gh-pages
 });
